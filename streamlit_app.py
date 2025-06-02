8f0fc977ebb2c7999c4bbca2301ffd63c385989f
Farelgantengz <mfarelzylann@gmail.com>
2025-06-02 11:30:52 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,10 @@ st.write(
     "AKHIRNYA BISA BIKIN AKUNNNN"
 )
 st.image("mantaprek.png")
+st.header("Pengecek Nilai Genap/Ganjil")
+angka = st.number_input("Masukkan Angka:", value=0, step=1)
+
+if (angka %2) == 0:
+    st.write(f"{angka} yaitu Bilangan Genap cuyy")
+else:
+    st.write(f"{angka} yaitu Bilangan Ganjil cuyy")
